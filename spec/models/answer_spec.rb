2d782796151383592cb0760fa51cc44c08b013df
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-05-15 09:44:35 -0700

    commit 2

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: true) }
+  let(:question) { Question.create!(title: "New Question Title", body: "New Body", resolved: true) }
    let(:answer) { Answer.create!(body: 'Comment Body', question: question) }
 
    describe "attributes" do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Question, type: :model do
        expect(question).to have_attributes(title: "New Post Title", body: "New Post Body", resolved: true)
      end
    end
+
+   it "should respond to title" do
+     expect(question).to respond_to (:title)
+   end
+
+   it "should respond to body" do
+     expect(question).to respond_to (:body)
+   end
+
+   it "should respond to resolved" do
+     expect(question).to respond_to (:resolved)
+   end
+
 end
