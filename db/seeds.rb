ba3add89cdc2cb402fe31b60ce0f7418075cf2a6
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-06-11 10:53:42 -0700

    checkpoint-30-voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,17 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
+   post = Post.create!(
     user: users.sample,
     topic:  topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
 posts = Post.all
 
@@ -93,3 +98,4 @@ puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
 puts "#{Topic.count} topics created"
 puts "#{SponsoredPost.count} sponsor_posts created"
+puts "#{Vote.count} votes created"
