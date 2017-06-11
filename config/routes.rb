fc1a2c5751e7589dee36283c096d8624e977c328
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-06-11 07:32:31 -0700

    checkpoint-29-comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,18 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   post 'users/confirm' => 'users#confirm'
 
     get 'about' => 'welcome#about'
-
     get "welcome/contact"
-
     get "welcome/faq"
-
     root 'welcome#index'
 
 end
